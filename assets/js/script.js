3e52760ac0c9abe45d0a8d58408edc5f8d948bd5
Chura33 <churabrady@gmail.com>
2023-03-06 00:20:23 +0100

    second-commit

---
+++ assets/js/script.js
@@ -0,0 +1,11 @@
+window.onload = function(){
+    window.addEventListener("scroll",(e)=>{
+        if(window.pageYOffset > 100){
+            document.querySelector('header').classList.add("is-scrolling");
+        }
+
+        else{
+            document.querySelector('header').classList.remove("is-scrolling");
+        }
+    })   
+}
